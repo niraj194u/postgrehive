0a4e4a8cb586f75e96d14727801b41ca45536d13
niraj194u <niraj994u@gmail.com>
2024-12-20 22:36:08 +0530

    done

--- src/postgresToHiveIncrementaldata.py
+++ src/postgresToHiveIncrementaldata.py
@@ -2,29 +2,26 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql import functions as F
 
-spark = SparkSession.builder.master("local").appName("nirajProject").enableHiveSupport().getOrCreate()
+spark = SparkSession.builder.master("local").appName("Incrementalhive").enableHiveSupport().getOrCreate()
 max_id = spark.sql("SELECT max(id) FROM bigdata_nov_2024.niraj_person")
 m_id = max_id.collect()[0][0]
 str(m_id)
 
-query = 'SELECT * FROM people WHERE "ID" > ' + str(m_id)
+query = 'SELECT * FROM person WHERE id > ' + str(m_id)
 
 more_data = spark.read.format("jdbc") \
-    .option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb") \
+    .option("url", "jdbc:postgresql://18.132.73.146:5432/testdb") \
     .option("driver", "org.postgresql.Driver") \
     .option("user", "consultants") \
     .option("password", "WelcomeItc@2022") \
     .option("query", query) \
     .load()
 
-# Added new column of email address 
-more_data_with_email = more_data.withColumn('email_address', F.concat(more_data['name'], F.lit('@gmail.com')))
 
 
-# Show the updated DataFrame with the new column
-more_data_with_email.show()
-
-more_data_with_email.write.mode("append").saveAsTable("bigdata_nov_2024.niraj_person")
+# Sort the DataFrame by ID
+more_data.show()
+more_data.write.mode("append").saveAsTable("bigdata_nov_2024.niraj_person")
 print("Successfully Load to Hive")
 
 # spark-submit --master local[*] --jars /var/lib/jenkins/workspace/nagaranipysparkdryrun/lib/postgresql-42.5.3.jar src/IncreamentalLoadPostgressToHive.py
